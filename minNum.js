2e7348741812ef0a8851be0b1639ed8fd121c424
Yurika Jacobsson <nakiruy@gmail.com>
2017-09-05 18:24:26 -0700

    Solve minNum

--- minNum.js
+++ minNum.js
@@ -1,15 +1,12 @@
 function minNum(A, K, P) {
-  let count = 0
-  if (A + P <= K) {
-      count++;
-      if (A + P === K) {
-          count++;
-      } 
-  } else if (A + P > K) {
-      count = -1;
+    let count = 0
+    if (K - A >= P) {
+        count++;
+    } else if (A + P > K) {
+        count = -1;
+    }
+    return count;
   }
-  return count;
-}
 
 console.log(minNum(3, 5, 1));
 console.log(minNum(4, 5, 1));
